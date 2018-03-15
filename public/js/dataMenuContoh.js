6e7ea55a95816cd1aaf9a89625485ad370249f3d
egodasa <egodasa@gmail.com>
2018-03-15 09:42:17 +0700

    Memisahkan isi menu ke file tersendiri (dataMenu.js di folder public/js)

---
+++ public/js/dataMenuContoh.js
@@ -0,0 +1,33 @@
+let dataMenu = {
+	data () {
+		return {
+			menu : [
+					{
+						url : '/',
+						icon : 'fa fa-dashboard',
+						text : 'Halaman Vue 0'
+					},
+					{
+						url : '/vue',
+						icon : 'fa fa-dashboard',
+						text : 'Halaman Vue 1'
+					},
+					{
+						url : '/vue2',
+						icon : 'fa fa-dashboard',
+						text : 'Halaman Vue 2'
+					},
+					{
+						url : '/form',
+						icon : 'fa fa-dashboard',
+						text : 'Contoh Form'
+					},
+					{
+						url : '/tabel',
+						icon : 'fa fa-dashboard',
+						text : 'Contoh Tabel'
+					}
+				]
+		}
+	}
+}
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -24,57 +24,12 @@
     let dataMix = {}
     </script>
     @yield('vuedata')
+	<!--Ganti dataMenu.js dibawah ini sesuai kebutuhan. Karena isi menu diletakkan difile dibawah ini.-->
+    <script src="{{asset('js/dataMenuContoh.js')}}"></script> 
     <script>
 	const app = new Vue({
 		el : "#app",
-		mixins : [dataMix],
-		data (){
-			return {
-				menu : [
-					{
-						url : '/',
-						icon : 'fa fa-dashboard',
-						text : 'Halaman Vue 0'
-					},
-					{
-						url : '/vue',
-						icon : 'fa fa-dashboard',
-						text : 'Halaman Vue 1'
-					},
-					{
-						url : '/vue2',
-						icon : 'fa fa-dashboard',
-						text : 'Halaman Vue 2'
-					},
-					{
-						url : '/form',
-						icon : 'fa fa-dashboard',
-						text : 'Contoh Form'
-					},
-					{
-						url : '/tabel',
-						icon : 'fa fa-dashboard',
-						text : 'Contoh Tabel'
-					},
-					{
-						icon : 'fa fa-dashboard',
-						text : 'contoh 2',
-						child : [
-							{
-								url : 'contoh2.1',
-								icon : 'fa fa-dashboard',
-								text : 'contoh 2.1'
-							},
-							{
-								url : 'contoh2.2',
-								icon : 'fa fa-dashboard',
-								text : 'contoh 2.2'
-							}
-						]
-					}
-				]
-			}
-		}
+		mixins : [dataMix,dataMenu]
 	})
 	</script>
     <script src="{{asset('js/app_adminlte.js')}}"></script>
