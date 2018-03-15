ac4fcd415f609d19955cd43ddf9f9d18bdb3f06c
egodasa <egodasa@gmail.com>
2018-03-16 00:33:41 +0700

    Penambahan komponen alert dan modal

--- public/js/dataMenuContoh.js
+++ public/js/dataMenuContoh.js
@@ -5,27 +5,59 @@ let dataMenu = {
 					{
 						url : '/',
 						icon : 'fa fa-dashboard',
-						text : 'Halaman Vue 0'
+						text : 'Halaman 1'
 					},
 					{
 						url : '/vue',
 						icon : 'fa fa-dashboard',
-						text : 'Halaman Vue 1'
+						text : 'Halaman 2'
 					},
 					{
 						url : '/vue2',
 						icon : 'fa fa-dashboard',
-						text : 'Halaman Vue 2'
+						text : 'Halaman 3'
 					},
 					{
 						url : '/form',
 						icon : 'fa fa-dashboard',
-						text : 'Contoh Form'
+						text : 'Contoh Form Modal'
 					},
 					{
 						url : '/tabel',
 						icon : 'fa fa-dashboard',
 						text : 'Contoh Tabel'
+					},
+					{
+						url : '/tabel',
+						icon : 'fa fa-dashboard',
+						text : 'Dropdown',
+						child : [
+							{
+								url : '/',
+								icon : 'fa fa-dashboard',
+								text : 'Halaman 1'
+								},
+								{
+									url : '/vue',
+									icon : 'fa fa-dashboard',
+									text : 'Halaman 2'
+								},
+								{
+									url : '/vue2',
+									icon : 'fa fa-dashboard',
+									text : 'Halaman 3'
+								},
+								{
+									url : '/form',
+									icon : 'fa fa-dashboard',
+									text : 'Contoh Form'
+								},
+								{
+									url : '/tabel',
+									icon : 'fa fa-dashboard',
+									text : 'Contoh Tabel'
+								}
+						]
 					}
 				]
 		}
