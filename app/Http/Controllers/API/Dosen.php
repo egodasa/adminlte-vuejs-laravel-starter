0fdffe8ba036bae1fa7b99ef2d56bcec466c3d03
egodasa <egodasa@gmail.com>
2018-03-15 10:59:08 +0700

    Fix masalah error pembagian 0, jika per_page dan page tidak di set

--- app/Http/Controllers/API/Dosen.php
+++ app/Http/Controllers/API/Dosen.php
@@ -14,6 +14,11 @@ class Dosen extends Controller
      * @return \Illuminate\Http\Response
      */
     public $tablePk = 'nidn';
+    public $validateQueryString = [
+		"per_page"		=> "bail|sometimes|numeric|min:1",
+		"page"		=> "bail|sometimes|numeric|min:1",
+		"sort"		=> ["bail","sometimes","regex:/[a-zA-Z0-9]{0,20}\|(asc|desc)/"]
+    ];
     public function index(Request $req)
     {
         $data = new \stdClass;
@@ -21,53 +26,63 @@ class Dosen extends Controller
 		$page = $req->query('page');
 		
 		$sort = $req->query('sort');
-		if(isset($sort)) $sort = explode('|',$sort);
 		
-		$base_kueri = DB::table('tbdosen');
-		$totalData = DB::table('tbdosen')->select(DB::raw('COUNT(1) as total'))->get()->first();
+		$validator = \Validator::make($req->query(), $this->validateQueryString);
+		if($validator->fails()){
+			$data->error = $validator->errors();
+			$data->status_code = "422";
+		}else{
+			if(isset($sort)) $sort = explode('|',$sort);
 		
-		if(isset($per_page) && !isset($page)) $page = 1;
-		else if(!isset($per_page) && isset($page)) $per_page = 10;
-		
-		$kueri = $base_kueri->
-			when($sort, function ($query) use ($sort) {
-						return $query->orderBy($sort[0], substr($sort[1],0,4));
-	                })->
-			when($per_page, function ($query) use ($per_page) {
-						$page = 1;
-						return $query->limit($per_page)->offset(0);
-	                })->
-			when($page, function ($query) use ($page) {
-						$per_page = 10;
-						return $query->limit(10)->offset((int)(10 * ($page-1)));
-	                });
-	                
-		$data->data = $kueri->get();
-		
-		$lastPage = ceil($totalData->total/(int)$per_page);
-		
-		$queryStringNext = [
-			"per_page"		=> $per_page,
-			"page"			=> $page+1 <= 1 ? null : $page+1,
-			"sort"			=> $sort
-		];
-		$queryStringPrev = [
-			"per_page"		=> $per_page,
-			"page"			=> $page-1 > $lastPage ? null : $page-1,
-			"sort"			=> $sort
-		];
-		
-		$data->pagination = [
-			"total"			=> $totalData->total,
-			"per_page"		=> (int)$per_page,
-			"current_page"	=> (int)$page,
-			"from"			=> (($page-1)*$per_page)+1,
-			"to"			=> ($page*$per_page),
-			"last_page"		=> $lastPage,
-			"next_page_url" => $queryStringNext['page'] == null ? null : strtok($_SERVER["REQUEST_URI"],'?').'?'.http_build_query($queryStringNext),
-			"prev_page_url" => $queryStringPrev['page'] == null ? null : strtok($_SERVER["REQUEST_URI"],'?').'?'.http_build_query($queryStringPrev)
-		];
-		return response()->json($data);
+			$base_kueri = DB::table('tbdosen');
+			$totalData = DB::table('tbdosen')->select(DB::raw('COUNT(1) as total'))->get()->first();
+			
+			if(isset($per_page) && !isset($page)) $page = 1;
+			else if(!isset($per_page) && isset($page)) $per_page = 10;
+			
+			$kueri = $base_kueri->
+				when($sort, function ($query) use ($sort) {
+							return $query->orderBy($sort[0], substr($sort[1],0,4));
+		                })->
+				when($per_page, function ($query) use ($per_page) {
+							$page = 1;
+							return $query->limit($per_page)->offset(0);
+		                })->
+				when($page, function ($query) use ($page) {
+							$per_page = 10;
+							return $query->limit(10)->offset((int)(10 * ($page-1)));
+		                });
+		                
+			$data->data = $kueri->get();
+			
+			if(isset($per_page) || isset($page)){ // Jika di url ada per_page atau page, maka buat respon paginasi
+				$lastPage = ceil($totalData->total/(int)$per_page);
+			
+				$queryStringNext = [
+					"per_page"		=> $per_page,
+					"page"			=> $page+1 <= 1 ? null : $page+1,
+					"sort"			=> $sort
+				];
+				$queryStringPrev = [
+					"per_page"		=> $per_page,
+					"page"			=> $page-1 > $lastPage ? null : $page-1,
+					"sort"			=> $sort
+				];
+				
+				$data->pagination = [
+					"total"			=> $totalData->total,
+					"per_page"		=> (int)$per_page,
+					"current_page"	=> (int)$page,
+					"from"			=> (($page-1)*$per_page)+1,
+					"to"			=> ($page*$per_page),
+					"last_page"		=> $lastPage,
+					"next_page_url" => $queryStringNext['page'] == null ? null : strtok($_SERVER["REQUEST_URI"],'?').'?'.http_build_query($queryStringNext),
+					"prev_page_url" => $queryStringPrev['page'] == null ? null : strtok($_SERVER["REQUEST_URI"],'?').'?'.http_build_query($queryStringPrev)
+				];
+			}
+			$data->status_code = "200";
+		}
+		return response()->json($data, $data->status_code);
     }
 
     /**
